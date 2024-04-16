afd7da1c8a4627b541fda2a973729aae89ec07b8
omid <omiddadvar94@gmail.com>
2024-04-16 21:11:03 +0330

    feat : Added Artist pivot entities

---
+++ Source/Core/Cinamaart.Domain/Entities/Pivots/MovieArtist.cs
@@ -0,0 +1,21 @@
+﻿using Cinamaart.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cinamaart.Domain.Entities.Pivots
+{
+    [Table("MovieArtists")]
+    public class MovieArtist : BaseEntity<long>
+    {
+        [ForeignKey("Movie")]
+        public int MovieId { get; set; }
+        public Movie Movie { get; set; }
+        [ForeignKey("Artist")]
+        public int ArtistId { get; set; }
+        public Artist Artist { get; set; }
+    }
+}
---
+++ Source/Core/Cinamaart.Domain/Entities/Pivots/TvSerieArtist.cs
@@ -0,0 +1,21 @@
+﻿using Cinamaart.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cinamaart.Domain.Entities.Pivots
+{
+    [Table("TvSerieArtists")]
+    public class TvSerieArtist : BaseEntity<long>
+    {
+        [ForeignKey("TVSerie")]
+        public int TvSerieId { get; set; }
+        public TvSerie TVSerie { get; set; }
+        [ForeignKey("Artist")]
+        public int ArtistId { get; set; }
+        public Artist Artist { get; set; }
+    }
+}
