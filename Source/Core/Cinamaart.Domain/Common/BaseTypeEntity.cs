eaa6817ee52f62373bcbd80ec747e4c7c5b481d1
omid <omiddadvar94@gmail.com>
2024-05-12 21:15:20 +0330

    fix : BaseTypeEntity class was in the wrong folder

--- Source/Core/Cinamaart.Domain/Common/BaseTypeEntity.cs
+++ Source/Core/Cinamaart.Domain/Common/BaseTypeEntity.cs
@@ -1,16 +1,18 @@
-﻿using Microsoft.EntityFrameworkCore;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Cinamaart.Domain.Common
 {
-    public class BaseTypeEntity : BaseEntity<Int16>
+    [NotMapped]
+    public class BaseTypeEntity : BaseEntity<int>
     {
-        [StringLength(100) , Unicode(false)]
+        [StringLength(100)]
         public string Name { get; set; }
     }
 }
