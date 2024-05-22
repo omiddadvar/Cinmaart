967dfffc1a2b43ed4ed3ef0a3b41c56b957ea632
omid <omiddadvar94@gmail.com>
2024-05-22 18:18:46 +0330

    refactor : working on architecture • Created Presentation layer • Confgiured project dependencies for WebApi project • Added SeriLog configurations to appsetting

--- Source/Presentation/Cinamaart.WebAPI/Program.cs
+++ Source/Presentation/Cinamaart.WebAPI/Program.cs
@@ -1,8 +1,21 @@
+using Cinamaart.Presentation;
+using Cinamaart.Persistence;
+using Cinamaart.Application;
+using Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddApplication();
+builder.Services.AddPersistence();
+builder.Services.AddPresentation();
+
+builder.Host.UseSerilog((context , config) =>{
+    config.ReadFrom.Configuration(context.Configuration);
+});
 
 var app = builder.Build();
 
@@ -13,5 +26,7 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.UseSerilogRequestLogging();
 
 app.Run();
