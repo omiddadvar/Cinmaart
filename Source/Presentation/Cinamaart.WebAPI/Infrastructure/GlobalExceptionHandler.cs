c61ea6e889546c30a9f8a6044c866438ba20e1a5
omid <omiddadvar94@gmail.com>
2024-06-07 21:59:05 +0330

    feat : Added GlobalExceptionHandler

---
+++ Source/Presentation/Cinamaart.WebAPI/Infrastructure/GlobalExceptionHandler.cs
@@ -0,0 +1,31 @@
+﻿using Cinamaart.WebAPI.Abstractions;
+using Microsoft.AspNetCore.Diagnostics;
+
+namespace Cinamaart.WebAPI.Infrastructure
+{
+    public class GlobalExceptionHandler : IExceptionHandler
+    {
+        private readonly ILogger<GlobalExceptionHandler> _logger;
+
+        public GlobalExceptionHandler(ILogger<GlobalExceptionHandler> logger)
+        {
+            _logger = logger;
+        }
+
+        public async ValueTask<bool> TryHandleAsync(HttpContext httpContext, Exception exception, CancellationToken cancellationToken)
+        {
+            _logger.LogError(exception , "Exception occured : {Message}" ,exception.Message);
+
+            httpContext.Response.ContentType = "application/json";
+            httpContext.Response.StatusCode = exception switch
+            {
+                BadHttpRequestException => StatusCodes.Status400BadRequest,
+                _ => StatusCodes.Status500InternalServerError
+            };
+            var result = WebserviceResult.Failure("GlobalException", exception.Message);
+
+            await httpContext.Response.WriteAsJsonAsync(result);
+            return true;
+        }
+    }
+}
