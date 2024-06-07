f2bcaa1225bf93016d975c11e989db5d6440838d
omid <omiddadvar94@gmail.com>
2024-06-07 21:53:06 +0330

    fix : BaseEntity Abstractions modified

--- Source/Core/Cinamaart.Domain/Common/BaseAuditableEntity.cs
+++ Source/Core/Cinamaart.Domain/Common/BaseAuditableEntity.cs
@@ -1,4 +1,4 @@
-﻿using Cinamaart.Domain.Common.Interfaces;
+﻿using Cinamaart.Domain.Abstractions;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations.Schema;
--- Source/Core/Cinamaart.Domain/Common/BaseEntity.cs
+++ Source/Core/Cinamaart.Domain/Common/BaseEntity.cs
@@ -1,11 +1,11 @@
-﻿using Cinamaart.Domain.Common.Interfaces;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Cinamaart.Domain.Abstractions;
 
 namespace Cinamaart.Domain.Common
 {
