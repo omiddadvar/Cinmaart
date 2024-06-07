7bf185a06280463bf10c0a3d1dda9fc43535e4cf
omid <omiddadvar94@gmail.com>
2024-06-07 21:54:39 +0330

    feat : used Result-pattern to avoid unnecessary Exception creation

---
+++ Source/Core/Cinamaart.Domain/Abstractions/Error.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cinamaart.Domain.Abstractions
+{
+    public sealed record Error(string Code , string? Description = null)
+    {
+        public static readonly Error None = new Error(string.Empty);
+
+        public static implicit operator Result(Error error) => Result.Failure(error);
+    }
+}
---
+++ Source/Core/Cinamaart.Domain/Abstractions/Result.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cinamaart.Domain.Abstractions
+{
+    public class Result
+    {
+        public Result(bool isSuccess , Error error)
+        {
+            if (isSuccess && error != Error.None ||
+               !isSuccess && error == Error.None)
+            {
+                throw new ArgumentException("Invalid error", nameof(error));
+            }
+            IsSuccess = isSuccess;
+            Error = error;
+        }
+        public bool IsSuccess { get; }
+        public Error Error { get; }
+
+        public static Result Success() => new Result(true, Error.None);
+        public static Result Failure(Error error) => new Result(false, error);
+    }
+}
