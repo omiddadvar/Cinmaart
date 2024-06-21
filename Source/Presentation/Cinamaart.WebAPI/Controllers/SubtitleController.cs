a15429d2feec02d038d75d3b67b58a77df24f354
omid <omiddadvar94@gmail.com>
2024-06-21 21:05:09 +0330

    feat : Added some functionality schema of SubtitleController

--- Source/Presentation/Cinamaart.WebAPI/Controllers/SubtitleController.cs
+++ Source/Presentation/Cinamaart.WebAPI/Controllers/SubtitleController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Cinamaart.WebAPI.Abstractions.Constants;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Cinamaart.WebAPI.Controllers
@@ -7,5 +9,35 @@ namespace Cinamaart.WebAPI.Controllers
     [ApiController]
     public class SubtitleController : ControllerBase
     {
+        [HttpGet("Movie/{id}")]
+        [Authorize]
+        public async Task<IActionResult> GetPaginatedSubtitlesByMovieId(int id)
+        {
+            throw new NotImplementedException();
+        }
+        [HttpGet("Episode/{id}")]
+        [Authorize]
+        public async Task<IActionResult> GetPaginatedSubtitlesByEpisodeId(int id)
+        {
+            throw new NotImplementedException();
+        }
+        [HttpPost]
+        [Authorize(Policy = PolicyNames.RequireSubtitleEditionAccess)]
+        public async Task<IActionResult> AddSubtitle()
+        {
+            throw new NotImplementedException();
+        }
+        [HttpPut]
+        [Authorize(Policy = PolicyNames.RequireSubtitleEditionAccess)]
+        public async Task<IActionResult> EditSubtitle()
+        {
+            throw new NotImplementedException();
+        }
+        [HttpDelete]
+        [Authorize(Policy = PolicyNames.RequireSubtitleEditionAccess)]
+        public async Task<IActionResult> RemoveSubtitle()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
