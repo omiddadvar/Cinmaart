aaf2934bd070ac16987d9b954faa7d2bc24fdb49
omid <omiddadvar94@gmail.com>
2024-06-21 22:43:41 +0330

    feat : Added Controllers to hanlde Episodes and Seasons

---
+++ Source/Presentation/Cinamaart.WebAPI/Controllers/EpisodeController.cs
@@ -0,0 +1,40 @@
+﻿using Cinamaart.WebAPI.Abstractions.Constants;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Cinamaart.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EpisodeController : ControllerBase
+    {
+        [HttpGet("Paginate")]
+        public async Task<IActionResult> GetPaginatedEpisodes()
+        {
+            throw new NotImplementedException();
+        }
+        [HttpPost]
+        [Authorize(Policy = PolicyNames.RequireContentEditionAccess)]
+
+        public async Task<IActionResult> AddEpisode()
+        {
+            throw new NotImplementedException();
+        }
+        [HttpPut]
+        [Authorize(Policy = PolicyNames.RequireContentEditionAccess)]
+
+        public async Task<IActionResult> EditEpisode()
+        {
+            throw new NotImplementedException();
+        }
+        [HttpDelete]
+        [Authorize(Policy = PolicyNames.RequireContentEditionAccess)]
+
+        public async Task<IActionResult> DeleteEpisode()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Source/Presentation/Cinamaart.WebAPI/Controllers/SeasonController.cs
@@ -0,0 +1,39 @@
+﻿using Cinamaart.WebAPI.Abstractions.Constants;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cinamaart.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SeasonController : ControllerBase
+    {
+        [HttpGet("Paginate")]
+        public async Task<IActionResult> GetPaginatedSeasons()
+        {
+            throw new NotImplementedException();
+        }
+        [HttpPost]
+        [Authorize(Policy = PolicyNames.RequireContentEditionAccess)]
+
+        public async Task<IActionResult> AddSeason()
+        {
+            throw new NotImplementedException();
+        }
+        [HttpPut]
+        [Authorize(Policy = PolicyNames.RequireContentEditionAccess)]
+
+        public async Task<IActionResult> EditSeason()
+        {
+            throw new NotImplementedException();
+        }
+        [HttpDelete]
+        [Authorize(Policy = PolicyNames.RequireContentEditionAccess)]
+
+        public async Task<IActionResult> DeleteSeason()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
