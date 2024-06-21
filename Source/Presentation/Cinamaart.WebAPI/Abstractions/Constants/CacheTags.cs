572e0579b1522e130a4bc104b122e39960b2c9bb
omid <omiddadvar94@gmail.com>
2024-06-21 14:38:37 +0330

    feat : Adding CacheTags for output cachings...

--- Source/Presentation/Cinamaart.WebAPI/Abstractions/Constants/CacheTags.cs
+++ Source/Presentation/Cinamaart.WebAPI/Abstractions/Constants/CacheTags.cs
@@ -3,5 +3,10 @@
     public static class CacheTags
     {
         public const string Genre = "Genre";
+        public const string Tag = "Tag";
+        public const string Role = "Role";
+        public const string Artist = "Artist";
+        public const string Movie = "Movie";
+        public const string TvSerie = "TvSerie";
     }
 }
