b250ca5dfa9e029292ac0fa453fda99156539a53
omid <omiddadvar94@gmail.com>
2024-06-28 18:31:32 +0330

    refactor : moving Migrations and DBContext DI from WebAPI to Persistence

--- Source/Infrastructure/Cinamaart.Persistence/DependencyInjection.cs
+++ Source/Infrastructure/Cinamaart.Persistence/DependencyInjection.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Cinamaart.Persistence.Contexts;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,6 +13,11 @@ namespace Cinamaart.Persistence
     {
         public static IServiceCollection AddPersistence(this IServiceCollection services)
         {
+            string Conn = "Server=.;Database=Cinamaart;Trusted_Connection=True;TrustServerCertificate=True;Connection Timeout=3000;User ID=sa;Password=Essi@358;Integrated Security=False;Command timeout=3000";
+            services.AddDbContext<MainDBContext>(
+                options => options.UseSqlServer(Conn));
+           // options => options.UseSqlServer("name=ConnectionStrings:DefaultConnection"));
+
             return services;
         }
     }
