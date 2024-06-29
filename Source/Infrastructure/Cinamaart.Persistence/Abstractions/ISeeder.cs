3790ee8f3b17aacf1a3bb377af441c4681f682cc
omid <omiddadvar94@gmail.com>
2024-06-29 17:49:10 +0330

    feat : Adding Seeder abstraction and utility class

---
+++ Source/Infrastructure/Cinamaart.Persistence/Abstractions/ISeeder.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cinamaart.Persistence.Abstractions
+{
+    public interface ISeeder
+    {
+        uint Order { get; }
+        void Seed();
+    }
+}
---
+++ Source/Infrastructure/Cinamaart.Persistence/Seeders/DataSeeder.cs
@@ -0,0 +1,45 @@
+﻿using Cinamaart.Persistence.Abstractions;
+using Cinamaart.Persistence.Contexts;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cinamaart.Persistence.Seeders
+{
+    public class DataSeeder
+    {
+        public static void SeedData(IHost app)
+        {
+            var scopedFactory = app.Services.GetService<IServiceScopeFactory>();
+            using (var scope = scopedFactory.CreateScope())
+            {
+                var dbContext = scope.ServiceProvider.GetService<MainDBContext>();
+                _CallAllSeeders(dbContext);
+            }
+        }
+        private static void _CallAllSeeders(MainDBContext dbContext)
+        {
+            var assembly = Assembly.GetAssembly(typeof(MainDBContext));
+            var seederClasses = assembly.GetTypes()
+                    .Where(t => typeof(ISeeder).IsAssignableFrom(t)
+                                && !t.IsInterface)
+                    .Select(t => Activator.CreateInstance(t , dbContext) as ISeeder)
+                    .OrderBy(s => s.Order)
+                    .ToArray();
+
+            if(seederClasses is not null)
+            {
+                foreach(var seeder in seederClasses)
+                {
+                    seeder?.Seed();
+                }
+            }
+        }
+    }
+}
