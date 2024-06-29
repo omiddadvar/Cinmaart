d62108f7801348819db43fc923583c38166dc087
omid <omiddadvar94@gmail.com>
2024-06-29 17:38:45 +0330

    feat : Added Enums for seeders 1. Country 2. DocType 3. Gender

---
+++ Source/Core/Cinamaart.Domain/Common/Enums/DocumentTypeEnum.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cinamaart.Domain.Common.Enums
+{
+    public enum DocumentTypeEnum
+    {
+        Unknown = 0,
+        SRT = 1,
+        JPG = 2,
+        JPEG = 3,
+        PNG = 4,
+        BMP = 5,
+        GIF = 6,
+        PDF = 7,
+        XLS = 8,
+        XLSX = 9
+    }
+}
---
+++ Source/Core/Cinamaart.Domain/Common/Enums/GenderEnum.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cinamaart.Domain.Common.Enums
+{
+    public enum GenderEnum
+    {
+        Female = 0,
+        Male = 1,
+        Unkown = 2
+    }
+}
