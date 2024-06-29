ecae6333229291fdf9c91e683c86489ee5228acf
omid <omiddadvar94@gmail.com>
2024-06-29 18:10:53 +0330

    trim : trimmed DI of Persistence layer

--- Source/Infrastructure/Cinamaart.Persistence/DependencyInjection.cs
+++ Source/Infrastructure/Cinamaart.Persistence/DependencyInjection.cs
@@ -1,9 +1,14 @@
-﻿using Cinamaart.Persistence.Contexts;
+﻿using Cinamaart.Application.Abstractions.Repositories;
+using Cinamaart.Domain.Abstractions;
+using Cinamaart.Persistence.Abstractions;
+using Cinamaart.Persistence.Contexts;
+using Cinamaart.Persistence.Repositories;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -13,11 +18,17 @@ namespace Cinamaart.Persistence
     {
         public static IServiceCollection AddPersistence(this IServiceCollection services)
         {
-            string Conn = "Server=.;Database=Cinamaart;Trusted_Connection=True;TrustServerCertificate=True;Connection Timeout=3000;User ID=sa;Password=Essi@358;Integrated Security=False;Command timeout=3000";
             services.AddDbContext<MainDBContext>(
-                options => options.UseSqlServer(Conn));
-           // options => options.UseSqlServer("name=ConnectionStrings:DefaultConnection"));
+              options => options.UseSqlServer("name=ConnectionStrings:DefaultConnection"));
+            
+            services.AddRepositories();
+            
+            return services;
+        }
 
+        public static IServiceCollection AddRepositories(this IServiceCollection services)
+        {
+            services.AddScoped<IArtistRepository, ArtistRepository>();
             return services;
         }
     }
