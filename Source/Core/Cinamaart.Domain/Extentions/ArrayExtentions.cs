02903db6a9b958f8dbfbf008f10d285cbd8361e4
omid <omiddadvar94@gmail.com>
2024-07-11 18:36:48 +0330

    feat : added IsNullOfEmpty extention methoss for Lists and Arrays

---
+++ Source/Core/Cinamaart.Domain/Extentions/ArrayExtentions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cinamaart.Domain.Extentions
+{
+    public static class ArrayExtentions
+    {
+        public static bool IsNullOrEmpty<T>(this T[] array)
+        {
+            if (array == null || array.Length == 0) return true;
+            return false;
+        }
+    }
+}
---
+++ Source/Core/Cinamaart.Domain/Extentions/ListExtentions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cinamaart.Domain.Extentions
+{
+    public static class ListExtentions
+    {
+        public static bool IsNullOrEmpty<T>(this List<T> array)
+        {
+            if (array == null || array.Count == 0) return true;
+            return false;
+        }
+    }
+}
