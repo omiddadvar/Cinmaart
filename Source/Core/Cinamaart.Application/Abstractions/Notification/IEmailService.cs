52f9e6167f332e97e9e91243fb9f2eb154d12e54
omid <omiddadvar94@gmail.com>
2024-08-16 18:36:31 +0330

    feat : Added EmailServvice Abstractions to project

---
+++ Source/Core/Cinamaart.Application/Abstractions/Notification/IEmailService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cinamaart.Application.Abstractions.Notification
+{
+    public interface IEmailService
+    {
+        Task SendEmailAsync(string emailAddress, string subject, string htmlMessage);
+    }
+}
---
+++ Source/Infrastructure/Cinamaart.Infrastructure/Services/Notification/EmailService.cs
@@ -0,0 +1,17 @@
+﻿using Cinamaart.Application.Abstractions.Notification;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cinamaart.Infrastructure.Services.Notification
+{
+    public class EmailService : IEmailService
+    {
+        public Task SendEmailAsync(string emailAddress, string subject, string htmlMessage)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
