b195c6090956948ba8478d67535a49927819afcf
omid <omiddadvar94@gmail.com>
2024-08-29 17:45:36 +0330

    feat : completing GenreController functionalites

--- Source/Presentation/Cinamaart.WebAPI/Controllers/GenreController.cs
+++ Source/Presentation/Cinamaart.WebAPI/Controllers/GenreController.cs
@@ -1,9 +1,17 @@
 ﻿using Cinamaart.Application.Abstractions.Constants;
+using Cinamaart.Application.Features.Genres.Commands.AddGenre;
+using Cinamaart.Application.Features.Genres.Commands.RemoveGenre;
+using Cinamaart.Application.Features.Genres.Commands.UpdateGenre;
+using Cinamaart.Application.Features.Genres.Queries.GetAllGeners;
+using Cinamaart.Application.Features.Genres.Queries.GetGenreById;
+using Cinamaart.Domain.Abstractions;
 using Cinamaart.WebAPI.Abstractions.Constants;
 using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.OutputCaching;
+using System.Threading;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
 
 namespace Cinamaart.WebAPI.Controllers
 {
@@ -13,36 +21,51 @@ namespace Cinamaart.WebAPI.Controllers
     {
         [HttpGet]
         [OutputCache(PolicyName = CachePolicyNames.OutputCacheWithAuth, Tags = [CacheTags.Genre])]
-        public async Task<IActionResult> GetAllGenres()
+        public async Task<IActionResult> GetAllGenres(CancellationToken cancellationToken = default)
         {
-            throw new NotImplementedException();
+            var query = new GetAllGenersQuery();
+            var data = await mediator.Send(query, cancellationToken);
+            return data.IsSuccess ? Ok(data) : BadRequest(data);
         }
         [HttpGet("{id}")]
         [OutputCache(PolicyName = CachePolicyNames.OutputCacheWithAuth, Tags = [CacheTags.Genre], VaryByQueryKeys = ["id"])]
-        public async Task<IActionResult> GetGenre(int id)
+        public async Task<IActionResult> GetGenre(int id, CancellationToken cancellationToken = default)
         {
-            throw new NotImplementedException();
+            var query = new GetGenreByIdQuery(id);
+            var data = await mediator.Send(query, cancellationToken);
+            return data.IsSuccess ? Ok(data) : BadRequest(data);
         }
         [HttpPost]
         [Authorize(Roles = RoleNames.Administrator)]
-        public async Task<IActionResult> AddGenre()
+        public async Task<IActionResult> AddGenre(AddGenreCommand command, CancellationToken cancellationToken = default)
         {
-            await cacheStore.EvictByTagAsync(CacheTags.Genre, CancellationToken.None);
-            return null;
+            var data  = await mediator.Send(command, cancellationToken);
+            return await _runAfterCommandOperation(data);
         }
         [HttpPut]
         [Authorize(Roles = RoleNames.Administrator)]
-        public async Task<IActionResult> UpdateGenre()
+        public async Task<IActionResult> UpdateGenre(UpdateGenreCommand command,CancellationToken cancellationToken = default)
         {
-            await cacheStore.EvictByTagAsync(CacheTags.Genre, CancellationToken.None);
-            return null;
+            var data = await mediator.Send(command, cancellationToken);
+            return await _runAfterCommandOperation(data);
         }
         [HttpDelete]
         [Authorize(Roles = RoleNames.Administrator)]
-        public async Task<IActionResult> RemoveGenre()
+        public async Task<IActionResult> RemoveGenre(RemoveGenreCommand command,CancellationToken cancellationToken = default)
         {
-            await cacheStore.EvictByTagAsync(CacheTags.Genre, CancellationToken.None);
-            return null;
+            var data = await mediator.Send(command, cancellationToken);
+            return await _runAfterCommandOperation(data);
+        }
+
+        private async Task<IActionResult> _runAfterCommandOperation(IBaseResult result)
+        {
+            if (result.IsSuccess)
+            {
+                await cacheStore.EvictByTagAsync(CacheTags.Genre, CancellationToken.None);
+                return Ok(result);
+            }
+            else
+                return BadRequest(result);
         }
     }
 }
