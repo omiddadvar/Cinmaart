2afa83cf1d1d8abba94502defd150391e315e843
omid <omiddadvar94@gmail.com>
2024-08-29 18:23:41 +0330

    feat : Added TagProfile from Tag-Automapper configs

---
+++ Source/Core/Cinamaart.Application/Common/Mappings/Profiles/TagProfile.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using Cinamaart.Application.Features.Tags;
+using Cinamaart.Application.Features.Tags.Commands.AddTag;
+using Cinamaart.Application.Features.Tags.Commands.UpdateTag;
+using Cinamaart.Domain.Entities.Types;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cinamaart.Application.Common.Mappings.Profiles
+{
+    public class TagProfile : Profile
+    {
+        public TagProfile()
+        {
+            CreateMap<Tag, TagDTO>();
+            CreateMap<AddTagCommand, Tag>();
+            CreateMap<UpdateTagCommand, Tag>();
+        }
+    }
+}
