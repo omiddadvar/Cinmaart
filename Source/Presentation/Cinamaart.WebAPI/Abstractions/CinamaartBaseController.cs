9a01ec5df848bd4ca5da279a05a010039cc05913
omid <omiddadvar94@gmail.com>
2024-08-29 18:18:04 +0330

    feat: Added CinamaartBaseController to add more functionalities to ControllerBase

---
+++ Source/Presentation/Cinamaart.WebAPI/Abstractions/CinamaartBaseController.cs
@@ -0,0 +1,26 @@
+﻿using Cinamaart.Domain.Abstractions;
+using Cinamaart.WebAPI.Abstractions.Constants;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.OutputCaching;
+
+namespace Cinamaart.WebAPI.Abstractions
+{
+    public class CinamaartBaseController(IOutputCacheStore? cacheStore = null) : ControllerBase
+    {
+        protected async Task<IActionResult> runAfterCommandOperationWithCacheReset(
+            IBaseResult result,
+            string? cacheTag = null
+            )
+        {
+            if (result.IsSuccess)
+            {
+                if(cacheStore is not null && 
+                        !string.IsNullOrEmpty(cacheTag))
+                    await cacheStore.EvictByTagAsync(cacheTag, CancellationToken.None);
+                return Ok(result);
+            }
+            else
+                return BadRequest(result);
+        }
+    }
+}
