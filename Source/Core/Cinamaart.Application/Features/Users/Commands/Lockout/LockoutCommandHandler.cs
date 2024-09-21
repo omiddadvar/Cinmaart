6ce645f4ca952dd3b53850022ec61c17e849b6cb
omid <omiddadvar94@gmail.com>
2024-09-21 18:58:01 +0330

    feat : Added user-LOCKOUT feature

---
+++ Source/Core/Cinamaart.Application/Features/Users/Commands/Lockout/LockoutCommandHandler.cs
@@ -0,0 +1,45 @@
+﻿using Cinamaart.Domain.Abstractions;
+using Cinamaart.Domain.Entities.Identity;
+using Cinamaart.Domain.Extentions;
+using Cinamaart.SharedKernel;
+using Cinamaart.SharedKernel.Resources;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Localization;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cinamaart.Application.Features.Users.Commands.Lockout
+{
+    internal class LockoutCommandHandler(
+            UserManager<User> userManager,
+            IStringLocalizer<StringResources> localizer,
+            ILogger<LockoutCommandHandler> logger
+        ) : IRequestHandler<LockoutCommand, Result<bool>>
+    {
+        public async Task<Result<bool>> Handle(LockoutCommand request, CancellationToken cancellationToken)
+        {
+            try
+            {
+                var user = await userManager.FindByIdAsync(request.UserId.ToString());
+                if (user is not null)
+                {
+                    var identityResult = await userManager.SetLockoutEnabledAsync(user, request.Enabled);
+                    return identityResult.ToStandardResult<bool>();
+                }
+                else
+                    return Result<bool>.Failure("Lockout.NotFound",
+                        localizer[LocalStringKeyword.User_NotFoundById, request.UserId]);
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex, "Error while removing user, userId = {userId}", request.UserId);
+                return Result<bool>.Failure("Lockout.Exception", ex.Message);
+            }
+        }
+    }
+}
--- Source/Presentation/Cinamaart.WebAPI/Controllers/UserController.cs
+++ Source/Presentation/Cinamaart.WebAPI/Controllers/UserController.cs
@@ -1,4 +1,6 @@
-﻿using Cinamaart.Application.Features.Users.Commands.Register;
+﻿using Cinamaart.Application.Abstractions.Constants;
+using Cinamaart.Application.Features.Users.Commands.Lockout;
+using Cinamaart.Application.Features.Users.Commands.Register;
 using Cinamaart.Application.Features.Users.Commands.RemoveUser;
 using Cinamaart.Application.Features.Users.Queries.GetUserById;
 using Cinamaart.WebAPI.Abstractions.Constants;
@@ -35,10 +37,28 @@ namespace Cinamaart.WebAPI.Controllers
             throw new NotImplementedException();
         }
         [HttpDelete("{id}")]
-        //[Authorize(Roles = RoleNames.Administrator)]
+        [Authorize(Roles = RoleNames.Administrator)]
         public async Task<IActionResult> RemoveUser(long id,CancellationToken cancellationToken = default)
         {
             var command = new RemoveUserCommand(id);
+            var result = await mediator.Send(command, cancellationToken);
+            return result.IsSuccess ? Ok(result) : BadRequest(result);
+        }
+        
+        [HttpPatch("{id}")]
+        [Authorize(Roles = RoleNames.Administrator)]
+        public async Task<IActionResult> LockoutUser(long id , CancellationToken cancellationToken = default)
+        {
+            var command = new LockoutCommand(id, Enabled: true);
+            var result = await mediator.Send(command, cancellationToken);
+            return result.IsSuccess ? Ok(result) : BadRequest(result);
+        }
+
+        [HttpPatch("{id}")]
+        [Authorize(Roles = RoleNames.Administrator)]
+        public async Task<IActionResult> CancelLockoutUser(long id, CancellationToken cancellationToken = default)
+        {
+            var command = new LockoutCommand(id, Enabled: false);
             var result = await mediator.Send(command, cancellationToken);
             return result.IsSuccess ? Ok(result) : BadRequest(result);
         }
