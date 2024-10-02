3659c058bbb99822e182657133900831c785ffe7
omid <dadvar@tazarv.com>
2024-10-02 16:32:49 +0330

    feat : New IdentityResultExtension to convert IdentityResult to WebServiceResult

---
+++ Source/Core/Cinamaart.Application/Extentions/IdentityResultExtensions.cs
@@ -0,0 +1,27 @@
+﻿using Cinamaart.Application.Abstractions;
+using Cinamaart.Domain.Abstractions;
+using Cinamaart.Domain.Extentions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cinamaart.Application.Extentions
+{
+    public static class IdentityResultExtensions
+    {
+        public static WebServiceResult<T> ToStandardWebServiceResult<T>(this IdentityResult identityResult)
+        {
+            var result = new WebServiceResult<T>(
+                StatusCodes.Status400BadRequest,
+                identityResult.Succeeded,
+                typeof(T) == typeof(bool) ? (T)(object)identityResult.Succeeded : default(T),
+                identityResult.Errors.ToErrors()
+                );
+            return result;
+        }
+    }
+}
