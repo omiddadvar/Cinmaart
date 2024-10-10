a93ed5536fa7fba625ec9cdf1d869e2326752cb4
omid <omiddadvar94@gmail.com>
2024-10-10 22:50:17 +0330

    feat : Added basic DocumentRepository

---
+++ Source/Core/Cinamaart.Application/Abstractions/Repositories/IDocumentRepository.cs
@@ -0,0 +1,13 @@
+﻿using Cinamaart.Application.Interfaces.Repositories;
+using Cinamaart.Domain.Entities;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cinamaart.Application.Abstractions.Repositories
+{
+    public interface IDocumentRepository : IGenericRepository<Document>;
+}
---
+++ Source/Infrastructure/Cinamaart.Persistence/Repositories/DocumentRepository.cs
@@ -0,0 +1,20 @@
+﻿using Cinamaart.Application.Abstractions.Repositories;
+using Cinamaart.Domain.Common.Enums;
+using Cinamaart.Domain.Entities;
+using Cinamaart.Domain.Entities.Identity;
+using Cinamaart.Domain.Entities.Pivots;
+using Cinamaart.Domain.Entities.Types;
+using Cinamaart.Persistence.Contexts;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cinamaart.Persistence.Repositories
+{
+    public class DocumentRepository(MainDBContext dBContext) : 
+        GenericRepository<Document>(dBContext), IDocumentRepository;
+}
