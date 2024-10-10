9e6c9e5253a14f4a34755220df2f754a2c32bcaa
omid <omiddadvar94@gmail.com>
2024-10-10 22:49:48 +0330

    feat : Added UserProfileRepository

---
+++ Source/Core/Cinamaart.Application/Abstractions/Repositories/IUserProfileRepository.cs
@@ -0,0 +1,17 @@
+﻿using Cinamaart.Domain.Entities.Pivots;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cinamaart.Application.Abstractions.Repositories
+{
+    public interface IUserProfileRepository
+    {
+        Task<UserDocument?> GetProfileUserDocument(long userId ,CancellationToken cancellationToken);
+        Task<bool> RemoveUserProfile(UserDocument userDocument, CancellationToken cancellationToken);
+        Task<UserDocument?> AddNewUserProfile(long userId , IFormFile formFile, CancellationToken cancellationToken);
+    }
+}
---
+++ Source/Infrastructure/Cinamaart.Persistence/Repositories/UserProfileRepository.cs
@@ -0,0 +1,95 @@
+﻿using Cinamaart.Application.Abstractions.Repositories;
+using Cinamaart.Application.Extentions.Files;
+using Cinamaart.Domain.Common.Enums;
+using Cinamaart.Domain.Entities;
+using Cinamaart.Domain.Entities.Pivots;
+using Cinamaart.Domain.Entities.Types;
+using Cinamaart.Persistence.Contexts;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cinamaart.Persistence.Repositories
+{
+    public class UserProfileRepository(MainDBContext dBContext) : IUserProfileRepository
+    {
+        public async Task<UserDocument?> AddNewUserProfile(long userId, IFormFile formFile, CancellationToken cancellationToken)
+        {
+            using var transaction = dBContext.Database.BeginTransaction();
+            try
+            {
+                var document = new Document
+                {
+                    Name = formFile.FileName,
+                    Extention = formFile.GetExtention(),
+                    SavedName = Guid.NewGuid().ToString(),
+                    DocumentTypeId = (int)formFile.GetDocType()
+                };
+                await dBContext.SaveChangesAsync();
+
+                var userdocument = new UserDocument
+                {
+                    UserId = userId,
+                    DocumentId = document.Id,
+                    UserDocumentTypeId = (int)UserDocumentTypeEnum.Profile
+                };
+
+                await dBContext.SaveChangesAsync();
+
+                transaction.Commit();
+
+                return userdocument;
+            }
+            catch
+            {
+                transaction.Rollback();
+                return null;
+            }
+        }
+
+        public async Task<UserDocument?> GetProfileUserDocument(long userId, CancellationToken cancellationToken)
+        {
+            var userDocument = await dBContext.UserDocuments
+                .FirstOrDefaultAsync(u => u.Id == userId
+                    && u.UserDocumentTypeId == (int)UserDocumentTypeEnum.Profile);
+            return userDocument;
+        }
+
+        public async Task<bool> RemoveUserProfile(UserDocument userDocument, CancellationToken cancellationToken)
+        {
+            using var transaction = dBContext.Database.BeginTransaction();
+            try
+            {
+                bool userDocumentExists = await dBContext
+                .UserDocuments
+                .AnyAsync(u => u.Id == userDocument.Id);
+
+                if (userDocumentExists)
+                {
+                    dBContext.UserDocuments.Remove(userDocument);
+                }
+
+                var document = await dBContext
+                    .Documents
+                    .FirstOrDefaultAsync(d => d.Id == userDocument.DocumentId);
+
+                if (document is not null)
+                {
+                    dBContext.Documents.Remove(document);
+                }
+
+                transaction.Commit();
+                return true;
+            }
+            catch
+            {
+                transaction.Rollback();
+                return false;
+            }
+        }
+    }
+}
